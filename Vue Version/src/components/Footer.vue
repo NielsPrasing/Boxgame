68c06d820fabf0de23762c2a3a1d86d599daa4fb
Niels Prasing <niels.prasing@outlook.com>
2020-08-11 07:49:09 +0200

    update sub for footer

--- Vue Version/src/components/Footer.vue
+++ Vue Version/src/components/Footer.vue
@@ -1,13 +1,16 @@
 <template>
-  <section class="col-12">
+  <div class="col-12">
     <p>
       <sub>I'm not responsible for actions caused by this game</sub>
     </p>
-  </section>
+  </div>
 </template>
 
 <style lang="scss">
 .col-12 {
   grid-column: span 12;
+}
+sub {
+  color: #212b2d;
 }
 </style>
